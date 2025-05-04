dce4203846a75312bd0dd05d53458bf7fc7dc75e
Rixit Dobariya <rixitdobariya05@gmail.com>
2025-05-04 22:01:59 +0530

    changed controller reference

--- server/src/routes/payment.js
+++ server/src/routes/payment.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder } = require("../controllers/paymentController");
+const { createOrder } = require("../controllers/payment.controller.js");
 
 router.post("/create-order", createOrder);
 
